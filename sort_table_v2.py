28221ff08519e8be768da4ff8312c5ac874e693d
Quaz123 <csi88978@cndps.com>
2020-10-23 11:41:50 +0200

    Update sort_table_v2.py

--- sort_table_v2.py
+++ sort_table_v2.py
@@ -1,8 +1,5 @@
 list_to_sort = [[3, 2, 3], [2, 0, 2], [3, 0, 1], [3, 1, 0]]
 
-list_to_sort.sort(key=lambda x: x[2])
-for i in range(0, len(list_to_sort) - 1):
-    if list_to_sort[i][1] > list_to_sort[i + 1][1]:
-        list_to_sort[i], list_to_sort[i + 1] = list_to_sort[i + 1], list_to_sort[i]
+list_to_sort.sort(key=lambda x: (x[1], x[2]))
 
 print(list_to_sort)
