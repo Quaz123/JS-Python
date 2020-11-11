2717ee175e693e747e6877c6dcb083f834c95967
Quaz123 <csi88978@cndps.com>
2020-11-11 16:52:47 +0100

    a

--- zadanie5.py
+++ zadanie5.py
@@ -1,5 +1,8 @@
+from random import randint
+
 a = ['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
 b = [a[i][0] for i in range(len(a))]
 print(b)
 
-c = []
+c = [[randint(0, 10) for i in range(4)] for j in range(5)]
+print(c)
